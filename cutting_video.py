ac3172f1c901059add32ea7a87cf15c136b2a6d3
Mohammadreza Ramzanpour <42400536+rezaramz@users.noreply.github.com>
2022-02-04 19:09:30 -0600

    Update cutting_video.py

--- cutting_video.py
+++ cutting_video.py
@@ -1,11 +1,20 @@
 import sys
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import argparse
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+parser = argparse.ArgumentParser()
 
-start_time = int(sys.argv[3])
-end_time = int(sys.argv[4])
+parser.add_argument('--output_file', '-o', help='The path the output video is written into.')
+parser.add_argument('--input_file', '-i', help='The path to the input video.')
+parser.add_argument('--start_time', '-s', help='The start time.')
+parser.add_argument('--end_time', '-e', help='The end time.')
+
+args = parser.parse_args()
+
+input_file = args.input_file
+output_file = args.output_file
+start_time = args.start_time
+end_time = args.end_time
 
 ffmpeg_extract_subclip(input_file, start_time, end_time, targetname=output_file)
 
